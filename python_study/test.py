b3a45eec344f3298f47844072d9a512d2ad331c1
anywhere-go <jwlee1112@gmail.com>
2023-04-21 11:06:01 +0900

    test2

--- python_study/test.py
+++ python_study/test.py
@@ -45,17 +45,36 @@
 # print("종료!")
 #결과는 종료! 출력됨
 
-yo_price = 1000
-yo_qty = 4
-milk_price = 1500
-milk_qty = 2
+# yo_price = 1000
+# yo_qty = 4
+# milk_price = 1500
+# milk_qty = 2
 
-yo_sales = yo_price * yo_qty
-milk_sales = milk_price * milk_qty
+# yo_sales = yo_price * yo_qty
+# milk_sales = milk_price * milk_qty
 
-total_sales =  yo_sales + milk_sales
-total_qty = yo_qty + milk_qty
+# total_sales =  yo_sales + milk_sales
+# total_qty = yo_qty + milk_qty
 
-print("드링킹 요구르트 매출액:" + str(yo_sales))
-print("딸기 우유 매출액:" + str(milk_sales))
-print("-")
+# print("드링킹 요구르트 매출액:" + str(yo_sales))
+# print("딸기 우유 매출액:" + str(milk_sales))
+# print("-")
+
+# coffee ="플랫화이트"
+# for x in coffee:
+#     print(x)
+#플
+#랫
+#화
+#이
+#트
+
+# count = [1,2,3,4,5,6,7,8,9,10]
+# for x in count:
+#     if x % 2 == 0:
+#         print(str(x) +"!")
+
+# 
+# order = ["아메리카노", "플랫 화이트", "화이트 초콜릿 모카"]
+# prince =[3100, 4100, 4600]
+# for x in range
